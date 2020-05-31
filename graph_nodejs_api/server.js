ff7a24bc947d20a06aee601a1d8ea56e80772ec4
Alexey <iloveyoucreator@gmail.com>
2020-05-31 19:24:56 +0300

    server added

--- graph_nodejs_api/server.js
+++ graph_nodejs_api/server.js
@@ -3,7 +3,15 @@ const app = express();
 
 const PORT = 9000;
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
+app.post('/api/nodejs/checkFull', (req, res) => {
+    
+});
 
 app.listen(PORT, function () {
     console.log('Listening on port ', PORT);
