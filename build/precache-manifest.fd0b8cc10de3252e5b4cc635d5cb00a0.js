60890a38e1a32d1f732a0b16a6ea2169c6b00e6b
Alexey <iloveyoucreator@gmail.com>
2020-05-31 19:02:16 +0300

    add build folder

---
+++ build/precache-manifest.fd0b8cc10de3252e5b4cc635d5cb00a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b3a7973e6bff8bf740189c079e7670d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0beddf7fd4573db8b4e1",
+    "url": "./static/js/2.39b31fc8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.39b31fc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d50e9f65a6c156f9128",
+    "url": "./static/js/main.c9e1f628.chunk.js"
+  },
+  {
+    "revision": "6b1dcd946c6500d80bd9",
+    "url": "./static/js/runtime-main.f7a8f145.js"
+  }
+]);
